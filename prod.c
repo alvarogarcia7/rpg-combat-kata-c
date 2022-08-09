86701650f5b6b0922dc1b009d0ac50a7bfa2b856
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2022-08-09 20:34:11 +0400

    Empty the skeleton

--- prod.c
+++ prod.c
@@ -2,16 +2,3 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-char *fizzBuzz(int number)
-{
-    if (15 == number)
-    {
-        return "FizzBuzz";
-    }
-    if (5 == number)
-    {
-        return "Buzz";
-    }
-    return "Fizz";
-}
